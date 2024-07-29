4876f83a8d445f4f32931064bae4c9ad046c8519
Yong Zhi <79264302+pohyongzhi@users.noreply.github.com>
2024-07-29 23:45:45 +0800

    Able to query now

--- index.js
+++ index.js
@@ -9,10 +9,33 @@ async function getWeatherInfo() {
     try {
         const response = await fetch(website);
         const data = await response.json();
-        console.log(data.days[0]);
 
         const icon = data.days[0].icon;
-        console.log(icon);
+        const temp = data.days[0].temp;
+        const conditions = data.days[0].conditions;
+        const feelsLike = data.days[0].feelslike;
+
+        // Get HTML elements
+        const weatherTemperatureTemp = document.querySelector(
+            ".weather-temperature-temp"
+        );
+        const weatherConditions = document.querySelector(".weather-conditions");
+        const weatherTemperatureFeelslike = document.querySelector(
+            ".weather-temperature-feelslike"
+        );
+        const weather = document.querySelector(".weather");
+
+        // Populate Information
+        weatherTemperatureTemp.textContent = temp + "°";
+        weatherConditions.textContent = conditions;
+        weatherTemperatureFeelslike.textContent =
+            "Feels like " + feelsLike + "°";
+
+        const img = document.createElement("img");
+        img.src = "assets/images/" + icon + ".svg";
+        img.alt = icon + "image";
+
+        weather.appendChild(img);
     } catch (error) {
         console.log(error);
     }
